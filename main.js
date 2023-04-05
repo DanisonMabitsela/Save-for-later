53e9bf32124bb411ad0e6ea0115a757b531102ea
Kwena Danison Mabitsela <xboxmabitsela@gmail.com>
2023-04-05 11:52:36 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,88 @@
+// Function to save an item
+async function saveItem(item) {
+  try {
+    // Get the saved items from the local storage
+    const savedItems = JSON.parse(localStorage.getItem("savedItems")) || [];
+
+    savedItems.push(item);
+
+    localStorage.setItem("savedItems", JSON.stringify(savedItems));
+
+    alert(
+      `${item} saved successfully! You have ${savedItems.length} items in your "Save for Later" folder.`
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+async function showSavedItems() {
+  try {
+    // Get the saved items and likes from the local storage
+    const savedItems = JSON.parse(localStorage.getItem("savedItems")) || [];
+    const savedLikes = JSON.parse(localStorage.getItem("savedLikes")) || [];
+    // Open the saved items page in a new window
+    const savedItemsWindow = window.open();
+
+    savedItemsWindow.document.write(`
+      <h1>Your Saved Items:</h1>
+      <ul>${savedItems
+        .map(
+          (item) => `
+          <li>${item} 
+            <button onclick="saveLike('${item}')">
+              ${savedLikes.includes(item) ? "Liked" : "Like"}
+            </button>
+          </li>`
+        )
+        .join("")}
+      </ul>
+      <form>
+        <label for="comment">Add Comment:</label><br>
+        <input type="text" id="comment" name="comment"><br>
+        <button type="button" onclick="saveCommentAndShowMessage(document.getElementById('comment').value)">Save Comment</button>
+      </form>
+      <form>
+        <label for="message">Send Message:</label><br>
+        <input type="text" id="message" name="message"><br>
+        <button type="button" onclick="sendMessageAndShowMessage(document.getElementById('message').value)">Send Message</button>
+      </form>
+    `);
+
+    // Define a function to display the success message
+    function showMessage(message) {
+      alert(message);
+    }
+
+    // Define a function to save a comment and display the success message
+    async function saveCommentAndShowMessage(comment) {
+      try {
+        const savedComments =
+          JSON.parse(localStorage.getItem("savedComments")) || [];
+        savedComments.push(comment);
+        localStorage.setItem("savedComments", JSON.stringify(savedComments));
+        showMessage(
+          `Comment saved successfully! You have ${savedComments.length} comments.`
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    // Define a function to send a message and display the success message
+    async function sendMessageAndShowMessage(message) {
+      try {
+        const savedMessages =
+          JSON.parse(localStorage.getItem("savedMessages")) || [];
+        savedMessages.push(message);
+        localStorage.setItem("savedMessages", JSON.stringify(savedMessages));
+        showMessage(
+          `Message sent successfully! You have ${savedMessages.length} messages.`
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
